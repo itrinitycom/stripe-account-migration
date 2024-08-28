7df52e68691539ce57f01cd854a2feb7335a4f9c
Miroslav Babják <mirek.babjak@gmail.com>
2024-08-28 14:21:57 +0200

    refactor: Check if product exists before copy

--- src/commands/copy-products.ts
+++ src/commands/copy-products.ts
@@ -2,6 +2,7 @@ import { promises as fs } from "fs";
 import { mapToCsvString } from "../core/csv";
 import { createStripeClient } from "../core/stripe";
 import { sanitizeProduct } from "./sanitize/product";
+import Stripe from "stripe";
 
 export async function copyProducts(
   filePath: string,
@@ -14,17 +15,23 @@ export async function copyProducts(
   await createStripeClient(apiKeyOldAccount)
     .products.search({ limit: 100, query: "active:'true'" })
     .autoPagingEach(async (oldProduct) => {
-        const newClient = createStripeClient(apiKeyNewAccount);
+      const newClient = createStripeClient(apiKeyNewAccount);
 
-        const product = await newClient.products.retrieve(oldProduct.id);
-        if (product) {
-            keyMap.set(oldProduct.id, product.id);
-            return;
-        }
+      let product: Stripe.Product | undefined;
+      try {
+        product = await newClient.products.retrieve(oldProduct.id);
+      } catch (error) {}
 
-        const newProduct = await newClient.products.create(sanitizeProduct(oldProduct));
+      if (product) {
+        keyMap.set(oldProduct.id, product.id);
+        return;
+      }
 
-        keyMap.set(oldProduct.id, newProduct.id);
+      const newProduct = await newClient.products.create(
+        sanitizeProduct(oldProduct)
+      );
+
+      keyMap.set(oldProduct.id, newProduct.id);
     });
 
   await fs.writeFile(filePath, await mapToCsvString(keyMap));
